03b7e6a77e39cde63eb721006103627ab409716f
Trey <contacttreyw@gmail.com>
2023-11-07 20:36:14 -0600

    Second Step

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("I brought my gears")
+var randomNumberGenerator = function (size) {
+    return Math.ceil(Math.random() * size);
+  }
+  
+  var questionGenerator = function () {
+    var question = {};
+    var num1 = randomNumberGenerator(10);
+    var num2 = randomNumberGenerator(10);
+    
+    question.answer = num1 + num2;
+    question.equation = String(num1) + " + " + String(num2);
+    
+    return question;
+  }
+
+ /*Test 
+ console.log(questionGenerator());
+  console.log(questionGenerator())
+  */
+
+  
